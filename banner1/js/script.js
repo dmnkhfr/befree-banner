99b69b34f774731b81a2950b8cae55abc9da9a4e
Dominik Hofer <admin@dominikhofer.me>
2019-01-09 10:53:29 +0100

    banner1 v1 finished

--- banner1/js/script.js
+++ banner1/js/script.js
@@ -1,15 +1,17 @@
-// Animation Timeline
+// timeline
 var animation = anime.timeline({
-    loop: false
+    loop: true
 });
 
-// Animations
+// animations
 animation
+    // logo
     .add({
         targets: '.logo',
         scale: [0, 1],
         easing: 'easeOutBack',
-        duration: 500
+        duration: 500,
+        delay: 200
     })
     .add({
         targets: '.logo .wing',
@@ -29,21 +31,91 @@ animation
         duration: 500,
         easing: 'easeInOutQuad'
     })
+    // url
+    .add({
+        targets: '.url',
+        translateY: '-20px',
+        opacity: 1,
+        duration: 200,
+        offset: '-=300',
+        easing: 'easeInOutQuad'
+    })
+    // title
     .add({
         targets: '.title',
         opacity: 1,
         translateY: '20px',
         duration: 200,
+        delay: 300,
+        easing: 'easeInOutQuad'
+    })
+    // acts
+    .add({
+        targets: '.act',
+        translateX: '310px',
+        delay: function(el, i, l) {
+            return i * 1400;
+        },
+        offset: '+=500',
+        duration: 1400,
+        easing: [0,.7,1,.3]
+    })
+    // overlay
+    .add({
+        targets: '.overlay',
+        translateX: '300%',
+        delay: 200,
+        duration: 1000,
+        easing: 'easeInOutQuad'
+    })
+    // remove stuff
+    .add({
+        targets: '.title',
+        opacity: 0,
+        duration: 10,
+        easing: 'linear',
+        offset: '-=600'
+    })
+    // date
+    .add({
+        targets: '.date',
+        opacity: 1,
+        translateY: '20px',
+        duration: 200,
+        delay: 300,
+        easing: 'easeInOutQuad'
+    })
+    // cta
+    .add({
+        targets: '.cta',
+        opacity: 1,
+        translateY: '-20px',
+        duration: 200,
         delay: 100,
         easing: 'easeInOutQuad'
     })
     .add({
-        targets: '.act',
-        translateX: '280px',
-        delay: function(el, i, l) {
-            return i * 1000;
-        },
+        targets: '.cta',
+        scale: [1, 1.3, .8, 1],
+        translateY: [0, 0],
+        duration: 600,
+        easing: 'easeInOutQuad',
+        delay: 2000 
+    })
+    // overlay
+    .add({
+        targets: '.overlay',
+        translateX: '0',
+        delay: 2000,
         duration: 1000,
         easing: 'easeInOutQuad'
+    })
+    // remove stuff
+    .add({
+        targets: ['.logo', '.date', '.cta', '.url'],
+        opacity: 0,
+        duration: 10,
+        offset: '-=500',
+        easing: 'linear'
     });
   
